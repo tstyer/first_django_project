9569cfc6c6fa54fafd5498439603378cae3eaa97
tstyer <travisstyer.ts@gmail.com>
2025-09-16 14:45:42 +0100

    Add a new view

--- hello_world/views.py
+++ hello_world/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+def about_me(request):
+    return HttpResponse("This would be the about page")
