d1adc14d69e0b36cdd17fc959e14675168c398d5
tstyer <travisstyer.ts@gmail.com>
2025-09-16 13:34:42 +0100

    Connect app to project in settings

--- my_project/urls.py
+++ my_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 from hello_world import views as index_views
 
 urlpatterns = [
+    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
