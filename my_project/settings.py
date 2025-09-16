7ef271b2f97b786d46b5f7631e69b3e15a139250
tstyer <travisstyer.ts@gmail.com>
2025-09-16 14:16:45 +0100

    Add 'about' app

--- my_project/settings.py
+++ my_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello, world',
+    'hello_world',
+    'about',
 ]
 
 MIDDLEWARE = [
