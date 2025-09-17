7d80f19525a947fab7fe9f1c5e3535aae7ba4b6a
tstyer <travisstyer.ts@gmail.com>
2025-09-17 11:11:50 +0100

    finish connecting about page.

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e*05#__90p!z&#vs87j55xq5j!_%bmd#blbt3ze13ud=8=j8_*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
